190d4060477b436799b6c992634fc7aac809ec55
r0xd4n3t <125836836+r0xd4n3t@users.noreply.github.com>
2023-11-10 16:43:03 +0800

    Handle KeyboardInterrupt gracefully to prevent crashes
    **Changes:**
    - Wrapped the main crawling loop in a try-except block to catch the KeyboardInterrupt exception.
    - Upon receiving a KeyboardInterrupt, the program now prints a message, performs cleanup tasks, and exits gracefully.
    
    **Reasoning:**
    This modification addresses the issue of the web crawler crashing when interrupted by a KeyboardInterrupt (Ctrl+C). By encapsulating the main crawling loop in a try-except block, the program can catch the KeyboardInterrupt exception and respond with a message indicating an interruption. Cleanup tasks are performed before exiting, ensuring a more graceful termination of the script. This enhancement improves the user experience and prevents potential data corruption or loss caused by abrupt program termination.

--- spiderword.py
+++ spiderword.py
@@ -59,63 +59,70 @@ def crawl(starting_url, base_url):
     # Add the starting URL to the set of URLs to crawl
     urls_to_crawl.add(starting_url)
 
-    while urls_to_crawl:
-        # Get the next URL from the set of URLs to crawl
-        url = urls_to_crawl.pop()
+    try:
+        while urls_to_crawl:
+            # Get the next URL from the set of URLs to crawl
+            url = urls_to_crawl.pop()
 
-        # Check if we have already visited this URL
-        if url in visited_urls:
-            continue
-        visited_urls.add(url)
+            # Check if we have already visited this URL
+            if url in visited_urls:
+                continue
+            visited_urls.add(url)
 
+            cleanup_wordlist()
+
+            # Set a random user agent for each request
+            headers = {'User-Agent': set_random_user_agent()}
+
+            # Make a request to the URL using the PoolManager object and get the HTML content
+            http = urllib3.PoolManager(cert_reqs='CERT_NONE')
+            response = http.request('GET', url, headers=headers)
+            html = response.data
+
+            try:
+                # Parse the HTML content using BeautifulSoup
+                soup = BeautifulSoup(html, 'html.parser')
+            except AssertionError:
+                # Handle parser errors and skip this URL
+                print(f"[!] Parser error: {url}")
+                continue
+
+            # Find all the text in the HTML content
+            text = soup.get_text()
+
+            # Split the text into words
+            words = re.findall(r"[a-zA-Z0-9]+", text)
+
+            # Remove any non-ASCII characters from the words
+            words = [word.encode('ascii', 'ignore').decode('utf-8') for word in words]
+
+            # Remove any duplicate words
+            unique_words = set(words)
+
+            # Write the unique words to a file called wordlist.txt
+            write_wordlist(unique_words)
+
+            # Count the number of unique words found
+            num_unique_words = len(unique_words)
+
+            # Find all the links on the page and add them to the set of URLs to crawl
+            for link in soup.find_all('a'):
+                next_url = link.get('href')
+                if next_url:
+                    absolute_url = urljoin(base_url, next_url)
+                    parsed_url = urlparse(absolute_url)
+                    if parsed_url.scheme == 'http' or parsed_url.scheme == 'https':
+                        if parsed_url.netloc == urlparse(base_url).netloc or parsed_url.netloc.endswith('.' + urlparse(base_url).netloc):
+                            urls_to_crawl.add(absolute_url)
+
+            # Print a progress message to indicate which page was crawled and the number of unique words found
+            print(f"[+] Crawled page: {url} [{num_unique_words} word(s) found]")
+
+    except KeyboardInterrupt:
+        print("\n[-] Crawling process interrupted. Cleaning up and exiting gracefully.")
         cleanup_wordlist()
-
-        # Set a random user agent for each request
-        headers = {'User-Agent': set_random_user_agent()}
-
-        # Make a request to the URL using the PoolManager object and get the HTML content
-        http = urllib3.PoolManager(cert_reqs='CERT_NONE')
-        response = http.request('GET', url, headers=headers)
-        html = response.data
-
-        try:
-            # Parse the HTML content using BeautifulSoup
-            soup = BeautifulSoup(html, 'html.parser')
-        except AssertionError:
-            # Handle parser errors and skip this URL
-            print(f"[!] Parser error: {url}")
-            continue
-
-        # Find all the text in the HTML content
-        text = soup.get_text()
-
-        # Split the text into words
-        words = re.findall(r"[a-zA-Z0-9]+", text)
-
-        # Remove any non-ASCII characters from the words
-        words = [word.encode('ascii', 'ignore').decode('utf-8') for word in words]
-
-        # Remove any duplicate words
-        unique_words = set(words)
-
-        # Write the unique words to a file called wordlist.txt
-        write_wordlist(unique_words)
-
-        # Count the number of unique words found
-        num_unique_words = len(unique_words)
-
-        # Find all the links on the page and add them to the set of URLs to crawl
-        for link in soup.find_all('a'):
-            next_url = link.get('href')
-            if next_url:
-                absolute_url = urljoin(base_url, next_url)
-                parsed_url = urlparse(absolute_url)
-                if parsed_url.scheme == 'http' or parsed_url.scheme == 'https':
-                    if parsed_url.netloc == urlparse(base_url).netloc or parsed_url.netloc.endswith('.' + urlparse(base_url).netloc):
-                        urls_to_crawl.add(absolute_url)
-
-        # Print a progress message to indicate which page was crawled and the number of unique words found
-        print(f"[+] Crawled page: {url} [{num_unique_words} word(s) found]")
+        # Add any additional cleanup tasks if needed
+        exit()
 
 # Read the starting URL from a file called url.txt
 with open('url.txt') as f:
